c01a5f33ede2fada51efc68fb8b222c7dac86f94
Sandro Jguburia <13996142+simboyd@users.noreply.github.com>
2019-03-01 03:32:19 +0400

    rename

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 // ayy lmao. this is the the SDL code template that I'll be working with.
 // compile with just bare-bones gcc main.c init.c -lSDL2 -lSDL2main -o game
 
-#include "init.h"
+#include "head.h"
 
 int main(int argc, char *argv[])
 {
